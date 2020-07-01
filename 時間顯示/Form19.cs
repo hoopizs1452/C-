345f832e3ee6c7a88e6b6c465f407a8de29d3fd1
hoopizs1452 <45253893+hoopizs1452@users.noreply.github.com>
2020-07-01 13:58:41 +0800

    update

---
+++ 時間顯示/Form19.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form19 : Form
+    {
+        public Form19()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.label1.Text = DateTime.Now.ToString();
+        }
+
+        private void Form19_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+    }
+}
