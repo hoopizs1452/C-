8d02d3a645d80d8f6918b3b4bf03ec3bdf9ff2aa
hoopizs1452 <45253893+hoopizs1452@users.noreply.github.com>
2020-07-02 10:00:51 +0800

    update

---
+++ 計算班級成績/Form22.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form22
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.LightGray;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6});
+            this.dataGridView1.Location = new System.Drawing.Point(34, 34);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 27;
+            this.dataGridView1.Size = new System.Drawing.Size(389, 186);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Column1
+            // 
+            this.Column1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.Column1.HeaderText = "班級";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // Column2
+            // 
+            this.Column2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.Column2.HeaderText = "1";
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 47;
+            // 
+            // Column3
+            // 
+            this.Column3.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.Column3.HeaderText = "2";
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 47;
+            // 
+            // Column4
+            // 
+            this.Column4.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.Column4.HeaderText = "3";
+            this.Column4.Name = "Column4";
+            this.Column4.Width = 47;
+            // 
+            // Column5
+            // 
+            this.Column5.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.Column5.HeaderText = "4";
+            this.Column5.Name = "Column5";
+            this.Column5.Width = 47;
+            // 
+            // Column6
+            // 
+            this.Column6.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.Column6.HeaderText = "平均";
+            this.Column6.Name = "Column6";
+            this.Column6.ReadOnly = true;
+            this.Column6.Width = 68;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.IndianRed;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("微軟正黑體", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button1.Location = new System.Drawing.Point(34, 235);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(139, 48);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "計算";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form22
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(804, 409);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form22";
+            this.Text = "計算班級成績";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ 計算班級成績/Form22.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form22 : Form
+    {
+        int[] stu_sum = { 3, 2, 4 };
+        public Form22()
+        {
+            InitializeComponent();
+            for(int i=0; i<3; i++)
+            {
+                dataGridView1.Rows.Add();
+                dataGridView1.Rows[i].Height = 25;
+            }
+
+            for (int i = 0; i < 5; i++)
+                dataGridView1.Columns[i].Width = 70;
+
+            dataGridView1.Rows[0].Cells[0].Value = "一";
+            dataGridView1.Rows[1].Cells[0].Value = "二";
+            dataGridView1.Rows[2].Cells[0].Value = "三";
+
+            dataGridView1.Width = dataGridView1.Columns[0].Width * 8;
+            dataGridView1.Height = dataGridView1.Rows[0].Height * 5;
+
+            dataGridView1.Columns[5].DefaultCellStyle.BackColor = Color.LightGray;
+            dataGridView1.Rows[0].Cells[4].ReadOnly = true;
+            dataGridView1.Rows[0].Cells[4].Style.BackColor = Color.LightGray;
+
+            for (int i=3; i<=4; i++)
+            {
+                dataGridView1.Rows[1].Cells[i].ReadOnly = true;
+                dataGridView1.Rows[1].Cells[i].Style.BackColor = Color.LightGray;
+            }
+
+            for (int i = 0; i < 3; i++)
+                for (int j = 1; j <= stu_sum[i]; j++)
+                    dataGridView1.Rows[i].Cells[j].Value = 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int [][] clas = new int[3][];
+            Single avg;
+
+            clas[0] = new int[3];
+            clas[1] = new int[2];
+            clas[2] = new int[4];
+
+            try
+            {
+                for (int i = 0; i < 3; i++)
+                    for (int j = 0; j < stu_sum[i]; j++)
+                        clas[i][j] = Convert.ToInt32(dataGridView1.Rows[i].Cells[j + 1].Value);
+            }
+            catch(Exception)
+            {
+                MessageBox.Show("輸入錯誤");
+                return;
+            }
+
+            for(int i=0; i<3; i++)
+            {
+                avg = 0;
+                for (int j = 0; j < stu_sum[i]; j++)
+                    avg += clas[i][j];
+
+                avg /= stu_sum[i];
+                dataGridView1.Rows[i].Cells[5].Value = avg;
+            }
+        }
+    }
+}
