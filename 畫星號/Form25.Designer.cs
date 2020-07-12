037e44bc43deb6607a06c8305651d66993b3faf6
hoopizs1452 <45253893+hoopizs1452@users.noreply.github.com>
2020-07-12 11:26:04 +0800

    update

---
+++ 畫星號/Form25.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form25
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.rowChoose = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.rowChoose)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微軟正黑體", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(30, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 30);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "列數:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(35, 130);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(147, 67);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // rowChoose
+            // 
+            this.rowChoose.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.rowChoose.Location = new System.Drawing.Point(103, 62);
+            this.rowChoose.Name = "rowChoose";
+            this.rowChoose.Size = new System.Drawing.Size(79, 34);
+            this.rowChoose.TabIndex = 2;
+            // 
+            // Form25
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(295, 240);
+            this.Controls.Add(this.rowChoose);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form25";
+            this.Text = "畫星號";
+            ((System.ComponentModel.ISupportInitialize)(this.rowChoose)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown rowChoose;
+    }
+}
---
+++ 畫星號/Form25.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form25 : Form
+    {
+        public Form25()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int row = (int)rowChoose.Value;
+            string n = "";
+
+            for(int i=1; i<=row; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                    n += "*";
+                n += "\r\n";
+            }
+
+            MessageBox.Show(n);
+        }
+    }
+}
