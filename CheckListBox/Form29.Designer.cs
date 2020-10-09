8c5106e5e85672129e0684bf2242b5fadfe9624a
hoopizs1452 <45253893+hoopizs1452@users.noreply.github.com>
2020-10-09 16:54:45 +0800

    update

---
+++ CheckListBox/Form29.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form29
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("微軟正黑體", 10F);
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(86, 33);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "新增項目";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Font = new System.Drawing.Font("微軟正黑體", 10F);
+            this.button2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button2.Location = new System.Drawing.Point(114, 13);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(146, 33);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "列出配勾選的項目1";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.Font = new System.Drawing.Font("微軟正黑體", 10F);
+            this.button3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button3.Location = new System.Drawing.Point(275, 13);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(146, 33);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "列出配勾選的項目2";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button4.Font = new System.Drawing.Font("微軟正黑體", 10F);
+            this.button4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button4.Location = new System.Drawing.Point(13, 65);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(146, 33);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "列出配勾選的項目3";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button5.Font = new System.Drawing.Font("微軟正黑體", 10F);
+            this.button5.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button5.Location = new System.Drawing.Point(176, 65);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(126, 33);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "哪個項目被點選";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button6.Font = new System.Drawing.Font("微軟正黑體", 10F);
+            this.button6.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button6.Location = new System.Drawing.Point(317, 65);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(104, 33);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "檢查項目狀態";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(200, 117);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(221, 184);
+            this.textBox1.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(13, 316);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 17);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "第O項的狀態:";
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.Font = new System.Drawing.Font("微軟正黑體", 10F);
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.IntegralHeight = false;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "Item1",
+            "Item2",
+            "Item3",
+            "Item4"});
+            this.checkedListBox1.Location = new System.Drawing.Point(16, 117);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(164, 184);
+            this.checkedListBox1.TabIndex = 8;
+            this.checkedListBox1.ItemCheck += new System.Windows.Forms.ItemCheckEventHandler(this.checkedListBox1_ItemCheck);
+            // 
+            // Form29
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(485, 430);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form29";
+            this.Text = "CheckListBox";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+    }
+}
---
+++ CheckListBox/Form29.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form29 : Form
+    {
+        public Form29()
+        {
+            InitializeComponent();
+        }
+
+        private void checkedListBox1_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            label1.Text = "第" + (e.Index + 1) + "項的狀態:" + e.NewValue.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            checkedListBox1.Items.Clear();
+
+            for (int i = 0; i < 4; i++)
+                checkedListBox1.Items.Add("Item " + (i + 1).ToString());
+
+            checkedListBox1.SetItemChecked(0, true);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string str;
+
+            textBox1.AppendText("-- 列出被勾選的項目，方法1 ---\r\n");
+            for(int i=0; i<=(checkedListBox1.Items.Count - 1); i++)
+                if(checkedListBox1.GetItemChecked(i))
+                {
+                    str = "第 " + (i + 1).ToString() + " 項:" + 
+                        checkedListBox1.Items[i].ToString()+ "\r\n";
+                    textBox1.AppendText(str);
+                }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string str = "";
+
+            textBox1.AppendText("-- 列出被勾選的項目，方法2 ---\r\n");
+            textBox1.AppendText("-共有" +
+                checkedListBox1.CheckedItems.Count + "被勾選\r\n");
+            foreach(var item in checkedListBox1.CheckedItems)
+            {
+                str = item.ToString() + "\r\n";
+                textBox1.AppendText(str);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.AppendText("-- 列出被勾選項目，方法3 --\r\n");
+            foreach (int item in checkedListBox1.CheckedIndices)
+                textBox1.AppendText("第 " + (item + 1).ToString() +
+                    " 項被勾選。\r\n");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            int i = checkedListBox1.SelectedIndex;
+            textBox1.AppendText("--判斷哪個項目被點選--\r\n");
+            textBox1.AppendText("第 " + (i + 1).ToString() +
+                "項目被點選\r\n");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox1.AppendText("---- 列出每項的勾選狀態 ----\r\n");
+            for (int i = 0; i < checkedListBox1.Items.Count; i++)
+                textBox1.AppendText("第 " + (i + 1).ToString() + " 項狀態:" +
+                    checkedListBox1.GetItemCheckState(i).ToString() + "\r\n");
+        }
+    }
+}
