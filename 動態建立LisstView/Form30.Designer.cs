b62096200cac2bf7d05e043486017836288df725
hoopizs1452 <45253893+hoopizs1452@users.noreply.github.com>
2021-01-21 14:29:53 +0800

    update

---
+++ 動態建立LisstView/Form30.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form30
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem("");
+            System.Windows.Forms.ListViewItem listViewItem2 = new System.Windows.Forms.ListViewItem("");
+            System.Windows.Forms.ListViewItem listViewItem3 = new System.Windows.Forms.ListViewItem("");
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form30));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.imageList2 = new System.Windows.Forms.ImageList(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.DimGray;
+            this.button1.FlatAppearance.BorderSize = 4;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button1.Location = new System.Drawing.Point(22, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(144, 47);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "1.建立圖示";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.button2.Enabled = false;
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.DimGray;
+            this.button2.FlatAppearance.BorderSize = 4;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button2.Location = new System.Drawing.Point(222, 25);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(144, 47);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "2.建立資料行";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.button3.Enabled = false;
+            this.button3.FlatAppearance.BorderColor = System.Drawing.Color.DimGray;
+            this.button3.FlatAppearance.BorderSize = 4;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button3.Location = new System.Drawing.Point(424, 25);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(144, 47);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "3.建立項目";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.CheckBoxes = true;
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            listViewItem1.StateImageIndex = 0;
+            listViewItem2.StateImageIndex = 0;
+            listViewItem3.StateImageIndex = 0;
+            this.listView1.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem1,
+            listViewItem2,
+            listViewItem3});
+            this.listView1.Location = new System.Drawing.Point(22, 103);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(550, 217);
+            this.listView1.SmallImageList = this.imageList1;
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.ItemChecked += new System.Windows.Forms.ItemCheckedEventHandler(this.listView1_ItemChecked);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(22, 342);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.textBox1.Size = new System.Drawing.Size(550, 203);
+            this.textBox1.TabIndex = 4;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "icon3.png");
+            this.imageList1.Images.SetKeyName(1, "icon4.png");
+            // 
+            // imageList2
+            // 
+            this.imageList2.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList2.ImageStream")));
+            this.imageList2.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList2.Images.SetKeyName(0, "icon7.png");
+            this.imageList2.Images.SetKeyName(1, "icon8.png");
+            // 
+            // Form30
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(621, 570);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form30";
+            this.Text = "Form30";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ImageList imageList2;
+        private System.Windows.Forms.ImageList imageList1;
+    }
+}
---
+++ 動態建立LisstView/Form30.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form30 : Form
+    {
+        public Form30()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String picPath;
+            Image img;
+
+            picPath = Application.ExecutablePath;
+            for (int i = 0; i < 4; i++)
+                picPath = System.IO.Directory.GetParent(picPath).ToString();
+            picPath += "\\picture\\";
+
+            img = Image.FromFile(picPath + "icon3.png");
+            imageList1.Images.Add(img);
+            img = Image.FromFile(picPath + "icon4.png");
+            imageList1.Images.Add(img);
+            listView1.SmallImageList = imageList1;
+
+            img = Image.FromFile(picPath + "icon7.png");
+            imageList2.Images.Add(img);
+            img = Image.FromFile(picPath + "icon8.png");
+            imageList2.Images.Add(img);
+            listView1.StateImageList = imageList2;
+
+            button1.Enabled = false;
+            button2.Enabled = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listView1.Columns.Add("狀態", 100);
+            listView1.Columns.Add("姓名", 120);
+            listView1.Columns.Add("住址", 150);
+            listView1.Columns.Add("電話", 100);
+
+            button2.Enabled = false;
+            button3.Enabled = true;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string[] name = { "張三郎", "陳美麗", "王曉明" };
+            string[] address = { "民生路225號", "愛國路1號", "中央路120號" };
+            string[] phone = { "22576267", "22661878", "0938123456" };
+
+            for(int i=0; i<3; i++)
+            {
+                listView1.Items[i].SubItems.Add(name[i]);
+                listView1.Items[i].SubItems.Add(address[i]);
+                listView1.Items[i].SubItems.Add(phone[i]);
+                listView1.Items[i].StateImageIndex = 0;
+
+                listView1.Items[0].ImageIndex = 0;
+                listView1.Items[1].ImageIndex = 1;
+                listView1.Items[2].ImageIndex = 0;
+
+                button3.Enabled = false;
+            }
+        }
+
+        private void listView1_ItemChecked(object sender, ItemCheckedEventArgs e)
+        {
+            textBox1.Clear();
+
+            foreach(ListViewItem item in listView1.CheckedItems)
+            {
+                foreach (ListViewItem.ListViewSubItem str in item.SubItems)
+                    textBox1.AppendText(str.Text.ToString() + " ");
+                textBox1.AppendText("\r\n");
+            }
+        }
+    }
+}
